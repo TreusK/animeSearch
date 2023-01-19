19663e96a0d4ca47e2da6fe3bcc04af6e56fceb4
TreusK <flutri@protonmail.com>
2023-01-18 21:22:42 -0300

    more css

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,24 +1,49 @@
 import './SingleCard.css';
+import {useState} from 'react';
 import { Card, ListGroup } from 'react-bootstrap';
 
 function SingleCard({ anime }) {
+    const [flip, setFlip] = useState(false);
+
     return (
-        <Card className='CardComp'>
-            <Card.Img variant="top" src={anime.images.webp.image_url} className='CardImgComp'/>
-            <Card.Body className='CardBodyComp'>
-                <Card.Text className='CardTxtComp'>{anime.title_english}</Card.Text>
-                <Card.Text className='CardTxtComp CardTxtCompJap'>{anime.title_japanese}</Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <ListGroup variant="flush">
-                    <ListGroup.Item className='listGroupItemComp' variant='light'>
-                        <p>score</p> <p>{anime.score}</p>
-                    </ListGroup.Item>
-                    <ListGroup.Item className='listGroupItemComp' variant='secondary'>
-                        <p>episodes</p> <p>{anime.episodes}</p>
-                    </ListGroup.Item>                
-                </ListGroup>
-            </Card.Footer>
+        <Card className='CardComp' border='light' bg={!flip ? "light" : "secondary"} text={!flip ? "dark" : "white"}  >
+            {!flip &&
+            <div className='front' onClick={() => setFlip(oldFlip => !oldFlip)}>
+                <Card.Img variant="top" src={anime.images.webp.image_url} className='CardImgComp'/>
+                <Card.Body className='CardBodyComp'>
+                    <Card.Text className='CardTxtComp'>{anime.title_english}</Card.Text>
+                    <Card.Text className='CardTxtComp CardTxtCompJap'>{anime.title_japanese}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item className='listGroupItemComp' variant={anime.score < 4 ? 'danger' : anime.score < 8 ? 'warning' : 'success'}>
+                            <p>score</p> <p>{anime.score}</p>
+                        </ListGroup.Item>
+                        <ListGroup.Item className='listGroupItemComp' variant='secondary'>
+                            <p>episodes</p> <p>{anime.episodes}</p>
+                        </ListGroup.Item>                
+                    </ListGroup>
+                </Card.Footer>
+            </div>}
+
+            {flip && 
+            <div onClick={() => setFlip(oldFlip => !oldFlip)}>
+                <Card.Img variant="top" src={anime.images.webp.image_url} className='CardImgComp'/>
+                <Card.Body className='CardBodyComp overflow'>
+                    <Card.Text className='CardTxtComp'>{(anime.synopsis == null || anime.synopsis.lenght == 0) ? 'No synopsis found' : anime.synopsis}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item className='listGroupItemComp' variant='dark'>
+                            <p>year</p> <p>{(anime.year == null || anime.year.lenght == 0) ? '---' : anime.year}</p>
+                        </ListGroup.Item>
+                        <ListGroup.Item className='listGroupItemComp overflow' variant='info'>
+                                <a href={anime.url} target='_blank'>My Anime List</a>
+                        </ListGroup.Item>                
+                    </ListGroup>
+                </Card.Footer>
+            </div>}
+            
         </Card>
     )
 }
